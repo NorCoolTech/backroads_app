2d181c6e2d41d66c4a2e9513ffcaed5e4cd86c2e
Norbert Kulacin <norbert.kulacin@gmail.com>
2023-02-13 09:02:23 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { pageLinks, sosicalLinks } from "../data";
+import PageLinks from "./PageLinks";
+import SocialLink from "./SocialLink";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {sosicalLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass="footer-icon" />;
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
